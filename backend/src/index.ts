02dbb0c5f9b01e3fcbfea295005a05606807b491
Marcos Felipe <p.marcosfelipe@gmail.com>
2021-06-19 23:54:22 +0100

    add ts-node-dev to transpile and run server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.send('Hello World');
+  return response.send('Hello World 2');
 });
 
 app.listen(3333);
