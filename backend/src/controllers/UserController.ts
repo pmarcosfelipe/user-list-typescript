e3d916ca983b4b058ef82dbe96c9bc150ae6e457
Marcos Felipe <p.marcosfelipe@gmail.com>
2021-06-20 00:54:32 +0100

    create a fake EmailService and a sendMail method and to use interface concept

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,13 +1,65 @@
 import { Request, Response } from 'express';
+import EmailService from '../services/EmailService';
+
 const users = [
   {
-    name: 'Marcos Felipe',
-    email: 'p.marcosfelipe@gmail.com',
+    name: 'Leanne Graham',
+    email: 'Sincere@april.biz',
+  },
+  {
+    name: 'Ervin Howell',
+    email: 'Shanna@melissa.tv',
+  },
+  {
+    name: 'Clementine Bauch',
+    email: 'Nathan@yesenia.net',
+  },
+  {
+    name: 'Patricia Lebsack',
+    email: 'Julianne.OConner@kory.org',
+  },
+  {
+    name: 'Chelsey Dietrich',
+    email: 'Lucio_Hettinger@annie.ca',
+  },
+  {
+    name: 'Mrs. Dennis Schulist',
+    email: 'Karley_Dach@jasper.info',
+  },
+  {
+    name: 'Kurtis Weissnat',
+    email: 'Telly.Hoeger@billy.biz',
+  },
+  {
+    name: 'Nicholas Runolfsdottir V',
+    email: 'Sherwood@rosamond.me',
+  },
+  {
+    name: 'Glenna Reichert',
+    email: 'Chaim_McDermott@dana.io',
+  },
+  {
+    name: 'Clementina DuBuque',
+    email: 'Rey.Padberg@karina.biz',
   },
 ];
 
 export default {
   async list(request: Request, response: Response) {
     return response.json(users);
+  },
+
+  async create(request: Request, response: Response) {
+    const emailService = new EmailService();
+
+    emailService.sendMail({
+      to: {
+        name: 'Marcos',
+        email: 'p.marcosfelipe@gmail.com',
+      },
+      message: { subject: 'Bem-vindo ao sistema', body: 'Seja bem-vindo' },
+    });
+
+    return response.send();
   },
 };
